ae0be11b16a9b4d3e1b2b3ce6b50e6a966dc6956
Christiaan <c.posthuma@student.han.nl>
2021-11-25 16:34:29 +0100

    made new template for the stock analyses
    -

---
+++ Stock_analistics.py
@@ -0,0 +1,10 @@
+
+def stock_checker():
+    print("i had to  make some changes")
+
+    # TODO: get basic info
+    # ticker_name, stock_price, company_name
+    # TODO: get advanced info
+    # Market_cap, PE_ratio, EPS, 1y_Target, Beta, outstanding_shares
+    # TODO: get more info
+    # 
